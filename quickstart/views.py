04379dcbcf90dfbe272127c6d503b85515cd0bba
wtlin1228 <wtlin0711@gmail.com>
2017-10-18 22:17:31 +0800

    fix upload api

--- quickstart/views.py
+++ quickstart/views.py
@@ -32,30 +32,6 @@ fields = {
 class FileUploadView(views.APIView):
     def post(self, request, format=None):
         try:
-
-            # file_obj = request.data['image']
-            # file_obj_copy = copy.deepcopy(file_obj)
-
-            # Imgur upload
-            # image_chunk = file_obj.read()
-            # url = imgurRequests(image_chunk)
-
-            # vgg model
-            # img = Image.open(file_obj_copy)
-            # process = preprocess(img)
-            # result_predict = vgg19(process)
-            # raw_city_rank = rank(result_predict)
-            # city_rank = [{ 'name': city_name, 'prob': prob } for city_name, prob in raw_city_rank]
-
-            # response = OrderedDict([
-            #     ['age', age],
-            #     ['gender', gender],
-            #     ['country', country],
-            #     ['city', city],
-            #     ['imgUrl', url],
-            # 	['rank', city_rank],
-            # ])
-
             body_unicode = request.body.decode('utf-8')
             body = json.loads(body_unicode)
 
@@ -64,24 +40,13 @@ class FileUploadView(views.APIView):
             country = body['country']
             city = body['city']
 
-            print(age, gender, country, city)
-
+            # TODO: url and rank should be replaced with the Imgur url and vgg model prediction
             url = 'https://i.imgur.com/Sc59JV1.jpg'
 
-            city_rank = [{"name":"Rio De Janeiro","prob":44.8},{"name":"Prague","prob":24.1},{"name":"Helsinki","prob":8.4},{"name":"Casablanca","prob":4.7},{"name":"Berlin","prob":3.0},{"name":"others:","prob":15.0}]
-            # city_rank_to_db = 'taiwan,50;china,50;'
+            # city_rank = [{"name":"Rio De Janeiro","prob":44.8},{"name":"Prague","prob":24.1},{"name":"Helsinki","prob":8.4},{"name":"Casablanca","prob":4.7},{"name":"Berlin","prob":3.0},{"name":"others:","prob":15.0}]
             city_rank_to_db = 'Rio De Janeiro,44.8;Prague,24.1;Helsinki,8.4;Casablanca,4.7;Berlin,3.0;others,15.0'
 
-            response = OrderedDict([
-                ['age', age],
-                ['gender', gender],
-                ['country', country],
-                ['city', city],
-                ['imgUrl', url],
-                ['rank', city_rank],
-            ])
-
-            User.objects.create(
+            user = User.objects.create(
                 age=age,
                 gender=gender,
                 country=country,
@@ -90,7 +55,9 @@ class FileUploadView(views.APIView):
                 rank=city_rank_to_db
             )
 
-            print(response)
+            response = {
+                'user': user.id,
+            }
 
             return Response(response, status=200)
         except Exception, e:
@@ -120,8 +87,6 @@ class GetUserView(generics.RetrieveAPIView):
             'imgUrl': user.imgUrl,
             'rank': obj,
         }
-
-        print(content)
 
         return Response(content, status=200)
 
