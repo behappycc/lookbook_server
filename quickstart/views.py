1f45925a77549e171ab4906ed2a5b8621473e8ff
wtlin1228 <wtlin0711@gmail.com>
2017-10-19 08:54:31 +0800

    try to get uploaded image data

--- quickstart/views.py
+++ quickstart/views.py
@@ -30,43 +30,44 @@ fields = {
 
 
 class FileUploadView(views.APIView):
-    def post(self, request, format=None):
+    def post(self, request, filename, format=None):
         try:
-            body_unicode = request.body.decode('utf-8')
-            body = json.loads(body_unicode)
-
-            age = body['age']
-            gender = body['gender']
-            country = body['country']
-            city = body['city']
-
-            file_obj = body['imgUrl'].encode('utf-8q')
-            print(file_obj)
-
-            # Imgur upload
-            image_chunk = file_obj.read()
-            print(image_chunk)
+            # body_unicode = request.body.decode('utf-8')
+            # body = json.loads(body_unicode)
+            #
+            # age = body['age']
+            # gender = body['gender']
+            # country = body['country']
+            # city = body['city']
 
             # TODO: url and rank should be replaced with the Imgur url and vgg model prediction
             url = 'https://i.imgur.com/Sc59JV1.jpg'
 
             # rank should be transform to this structure
-            city_rank_to_db = 'Rio De Janeiro,44.8;Prague,24.1;Helsinki,8.4;Casablanca,4.7;Berlin,3.0;others,15.0'
+            # city_rank_to_db = 'Rio De Janeiro,44.8;Prague,24.1;Helsinki,8.4;Casablanca,4.7;Berlin,3.0;others,15.0'
 
-            user = User.objects.create(
-                age=age,
-                gender=gender,
-                country=country,
-                city=city,
-                imgUrl=url,
-                rank=city_rank_to_db
-            )
+            # user = User.objects.create(
+            #     age=age,
+            #     gender=gender,
+            #     country=country,
+            #     city=city,
+            #     imgUrl=url,
+            #     rank=city_rank_to_db
+            # )
+            #
+            # response = {
+            #     'user': user.id,
+            # }
+            #
+            # return Response(response, status=200)
 
-            response = {
-                'user': user.id,
-            }
 
-            return Response(response, status=200)
+            # TODO: how to get the image like the master branch does!?!?
+            # QAQ
+            # QAQ
+
+
+            return Response({'user': 10}, status=200)
         except Exception, e:
             print e
             return Response({'message': 'Error occurs'}, status=500)
